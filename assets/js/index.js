32e19308f8e16cfc62aa426008d203362ddca7e6
DanielMtzP <danien888@hotmail.com>
2022-08-01 14:52:54 -0500

    menu hamburguesa terminado  y links funcionales

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,25 @@
-const enlace = document.createElement('A')
+const hamburguer = document.querySelector(".hamburguer")
 
-enlace.textContent = 'inicio'
+const navmenu = document.querySelector(".nav_menu")
+
+hamburguer.addEventListener("click",() => {
+    navmenu.classList.toggle('visible')
+})
+
+const home = document.querySelector(".h")
+const skills = document.querySelector(".s")
+const proyects = document.querySelector(".p")
+const contact = document.querySelector(".c")
+
+home.addEventListener("click",() => {
+    navmenu.classList.toggle('visible')
+})
+skills.addEventListener("click",() => {
+    navmenu.classList.toggle('visible')
+})
+proyects.addEventListener("click",() => {
+    navmenu.classList.toggle('visible')
+})
+contact.addEventListener("click",() => {
+    navmenu.classList.toggle('visible')
+})
